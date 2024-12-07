e641665585a957c2bf8879549296977740d02b1f
Xtian69420 <signo.303167@fairview.sti.edu.ph>
2024-12-07 20:39:56 +0800

    update booking email

--- User/LoggedIn/User-Function.js/ConfirmReservation-Functions.js
+++ User/LoggedIn/User-Function.js/ConfirmReservation-Functions.js
@@ -2,8 +2,19 @@ const urlParams = new URLSearchParams(window.location.search);
 const UnitId = urlParams.get('UnitId');
 const Reference = urlParams.get('Reference');
 const UserId = urlParams.get('UserId');
+let email_;
+let amount_;
+let modeOfPayment_;
+let unitName_;
+let checkIn_;
+let checkOut_;
+let typeOfPayment_;
+
 var Reservation;
 var FullPayment;
+
+// email = UserId.email
+// amount from method
 
 const errorMessage = document.getElementById('error-message');
 errorMessage.style.display = 'none'; 
@@ -15,6 +26,13 @@ function loadBookingViewData() {
             if (data) {
                 console.log(data);
                 const user = data;
+
+                email_ = user.UserId.email;
+                unitName_ = user.UnitId.unitName;
+
+                checkIn_ = user.CheckIn instanceof Date ? user.CheckIn.toISOString().split('T')[0] : new Date(user.CheckIn).toISOString().split('T')[0];
+                checkOut_ = user.CheckOut instanceof Date ? user.CheckOut.toISOString().split('T')[0] : new Date(user.CheckOut).toISOString().split('T')[0];
+                
 
                 document.getElementById('unit-name').innerHTML = `<strong>${user.UnitId.unitName}</strong>`;
                 document.getElementById('unit-loc').textContent = user.UnitId.location;
@@ -173,7 +191,7 @@ document.getElementById('pay-btn').onclick = function () {
 
 
 function getPaymentDetails() {
-
+    openLoading();
     const payLink = localStorage.getItem('payLink');
     
     if (payLink) {
@@ -182,87 +200,77 @@ function getPaymentDetails() {
             .then(response => response.json())
             .then(data => {
                 if (data) {
-
                     console.log('Payment details:', data);
+                    modeOfPayment_ = data.paymentDetails && data.paymentDetails.Mop ? data.paymentDetails.Mop : 'N/A';
+                    typeOfPayment_ = data.paymentDetails && data.paymentDetails.Description ? data.paymentDetails.Description : 'N/A';        
+                    amount_ = data.paymentDetails && data.paymentDetails.Amount ? data.paymentDetails.Amount : 'N/A';  
+                    sendEmail(amount_, modeOfPayment_, typeOfPayment_);
+                    closeLoading3()
                     window.location.href= `My-Bookings-View.html?id1=${Reference}&id=${UnitId}`
                 } else {
                     console.error('No data received from the API.');
+                    closeLoading3()
                 }
             })
             .catch(error => {
                 console.error('Error fetching payment details:', error);
+                closeLoading3()
             });
     } else {
         console.error('Payment link not found in localStorage.');
+        closeLoading3()
     }
 }
+function sendEmail(Amount, modeOfPayment, typeOfPayment) {
+    const email = email_;
+    const amount = Amount;
+    const typeOfPayment1 = typeOfPayment;
+    const methodOfPayment = modeOfPayment;
+    const unitName = unitName_;
+    const ci = checkIn_;
+    const co = checkOut_;
 
-function sendEmail(Amount){
-    fetch(`https://betcha-booking-api-master.onrender.com/booking/${Reference}`)
-    .then(response => response.json())
+    console.log('email:', email)
+    console.log('amount:', amount)
+    console.log('typeOfPayment:', typeOfPayment1)
+    console.log('methodOfPayment:', methodOfPayment)
+    console.log('unitName:', unitName)
+    console.log('ci:', ci)
+    console.log('co:', co)
+    const requestBody = {
+        email: email,
+        amount: amount,
+        typeOfPayment: typeOfPayment1,
+        methodOfPayment: methodOfPayment,
+        unitName: unitName,
+        checkIn: ci,
+        checkOut: co
+    };
+
+    console.log(JSON.stringify(requestBody));
+    fetch('https://betcha-booking-api-master.onrender.com/otp/bookMail', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(requestBody)
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+    })
     .then(data => {
-        if (data) {
-            console.log(data);
-            const user = data;
-            
-            document.getElementById('unit-name').innerHTML = `<strong>${user.UnitId.unitName}</strong>`;
-            document.getElementById('unit-loc').textContent = user.UnitId.location;
-
-            document.getElementById('unit-price').textContent = user.UnitId.unitPrice;
-            document.getElementById('num-of-days').textContent = user.NumOfDays;
-
-            const totalUnitPrice = user.UnitId.unitPrice * user.NumOfDays;
-            document.getElementById('up-nod').textContent = totalUnitPrice;
-
-            document.getElementById('ppc').textContent = user.UnitId.pricePerPax;
-            document.getElementById('addpax').textContent = user.AdditionalPax;
-
-            const additionalPaxPrice = user.UnitId.pricePerPax * user.AdditionalPax;
-            document.getElementById('ppc-addpax').textContent = additionalPaxPrice;
-
-            document.getElementById('reservationFee').textContent = user.UnitId.reservation;
-            document.getElementById('total-price').textContent = `₱${user.Total}`;
-
-            Reservation = user.UnitId.reservation;
-            FullPayment = user.Total
-
-            const unitImageElement = document.getElementById('unit-image');
-
-            if (user.UnitId && user.UnitId.UnitImages && user.UnitId.UnitImages.length > 0) {
-                const fileId = user.UnitId.UnitImages[0].fileId;
-                unitImageElement.src = `https://drive.google.com/thumbnail?id=${fileId}&sz=w1920-h1080`;
-                unitImageElement.alt = user.UnitId.unitName || 'Unit Image';
-            } else {
-                console.error("UnitImages is not available or empty.");
-
-                unitImageElement.src = '/path/to/default-image.jpg';
-                unitImageElement.alt = 'Default Unit Image';
-            }
-
-            const checkbox = document.getElementById('formCheck-1');
-            const button = document.getElementById('btn-confirm-pay');
-
-            checkbox.addEventListener('change', () => {
-                if (checkbox.checked) {
-
-                    button.removeAttribute('disabled');
-                    button.removeAttribute('style');
-                } else {
-
-                    button.disabled = true;
-                    button.style.backgroundColor = '#147B42'; 
-                }
-            });
-
-            if (!checkbox.checked) {
-                button.disabled = true;
-                button.style.backgroundColor = '#147B42'; 
-            }
+        if (data && data.success) {
+            console.log('Email sent successfully:', data);
+        } else {
+            console.error('Failed to send email:', data ? data.error || 'No error details provided' : 'No response data');
         }
     })
-    .catch(error => console.error('Error loading booking data:', error));
-
-
+    .catch(error => {
+        console.error('Error with the request:', error);
+    });    
 }
 
 document.getElementById('fetch-btn').addEventListener('click', getPaymentDetails);
